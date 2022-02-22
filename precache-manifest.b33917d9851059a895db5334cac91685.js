a69089f56c1d255f632b27a405ab38ceb208b1b3
Rubens Brandas <rubens.brandas@gmail.com>
2022-02-22 07:12:26 -0300

    Updates

---
+++ precache-manifest.b33917d9851059a895db5334cac91685.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ef1fc6cca1dab56dc4900f5755a65b",
+    "url": "/teslaui-clone/index.html"
+  },
+  {
+    "revision": "52e789999691540cbac8",
+    "url": "/teslaui-clone/static/js/2.0b849b48.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/teslaui-clone/static/js/2.0b849b48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e4591da20e334c7b22",
+    "url": "/teslaui-clone/static/js/main.13636271.chunk.js"
+  },
+  {
+    "revision": "4604affee7a6fa22b4c7",
+    "url": "/teslaui-clone/static/js/runtime-main.e47bd2d1.js"
+  }
+]);
